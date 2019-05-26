9cee1f6818b660477912c512c4b40e308ed86ea2
El maic <papa@Adrians-MacBook-Pro.local>
2019-05-26 00:31:01 -0500

    fuentes sin Mayus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,5 +28,20 @@ $("#bienvenida").modal({
 
 
 </script>
+
+<style type="text/css">
+	
+	@font-face {
+  font-family: "ClanOt-Bold";
+  src: url("{{ asset('clientlibs/fonts/clanot-bold.eot') }}");
+  src: url("{{ asset('clientlibs/fonts/clanot-bold.eot?') }}") format("embedded-opentype"), url("{{ asset('clientlibs/fonts/clanot-bold.woff') }}") format("woff"), url("{{ asset('clientlibs/fonts/clanot-bold.ttf') }}") format("truetype"), url("{{ asset('clientlibs/fonts/clanot-bold.svg#IDdelafuente') }}") format("svg");
+}
+@font-face {
+  font-family: "ClanOt-Medium";
+  src: url("{{ asset('clientlibs/fonts/clanot-medium.eot') }}");
+  src: url("{{ asset('clientlibs/fonts/clanot-medium.eot?') }}") format("embedded-opentype"), url("{{ asset('clientlibs/fonts/clanot-medium.woff') }}") format("woff"), url("{{ asset('clientlibs/fonts/clanot-medium.ttf') }}") format("truetype"), url("{{ asset('clientlibs/fonts/clanot-medium.svg#IDdelafuente') }}") format("svg");
+}
+
+</style>
 @endsection
 
