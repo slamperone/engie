d29562fb35ebfc1d4815ccff7f898292fc657238
El maic <papa@Adrians-MacBook-Pro.local>
2019-05-25 22:07:09 -0500

    tipografia gral & copy del mail

--- resources/views/mail/registro.blade.php
+++ resources/views/mail/registro.blade.php
@@ -10,7 +10,9 @@
 		<img src="https://engietepremia.com.mx/clientlibs/img/engie.png" width="200">
 	</div>
 	<h2 style="margin: 20px 0 60px; font-family: Gotham, 'Helvetica Neue', Helvetica, Arial, 'sans-serif'; font-size: 20px;">Hola {{ $nombre }}, gracias por registrarte en <strong>ENGIE te premia</strong></h2>    
-    <p style="font-family: Gotham, 'Helvetica Neue', Helvetica, Arial, 'sans-serif'">Tu numero de folio es: <b>{{ $folio }}</b>, debes conservarlo para canjear tu premio, recuerda que los ganadores serán dados a conocer el día 15 de julio del presente año. <br />Al ser este un email de confirmación, no es necesario que lo respondas.</p>
+    <p style="font-family: Gotham, 'Helvetica Neue', Helvetica, Arial, 'sans-serif'">Tu número de folio es: <b>{{ $folio }}</b>, debes conservarlo para canjear tu premio, recuerda que los ganadores serán dados a conocer el día 15 de julio del presente año. <br />Al ser este un email de confirmación, no es necesario que lo respondas. Si tienes alguna duda del concurso puedes escribirnos a registro@engietepremia.com.mx.
+¡</p>
+
     <p> ¡Te deseamos mucha suerte!</p>
 
 </div>
