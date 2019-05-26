cf215ef0baa3d32b21e59892f04d29e07f284b6c
El maic <papa@Adrians-MacBook-Pro.local>
2019-05-25 23:51:28 -0500

    cambio rutas fuentes

--- public/_clientlibs/scripts/header/script.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){
-        $('header').addClass("sticky-header");
-    }
-    else{
-        $('header').removeClass("sticky-header");
-    }
-});
